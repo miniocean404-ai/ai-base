6c88294fb21c7255440492cf020d5dc96c205968
MiniOcean404 <1037306928@qq.com>
2025-11-28 23:22:07 +0800

    ✨ feat: ocr 模型

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# OCR 视频
 原文视频: https://www.douyin.com/user/MS4wLjABAAAAnVCQPCy6KOdAxfj6LJj4DDFyknf74LQQUMVjYA8PLL-jPrgQpcgSScTEo2s3nS1V?from_tab_name=main&modal_id=7540988633839406362
+视频 2: https://www.bilibili.com/video/BV1ypUkB7Eki/?spm_id_from=333.1007.top_right_bar_window_history.content.click&vd_source=d898d1900c1ac2cfbd6d235613e095d3
---
+++ src/ocr/upload_hf.py
@@ -0,0 +1,16 @@
+from huggingface_hub import HfApi, upload_file
+
+from ocr.main import MnistModel
+
+
+def upload_model(model: MnistModel):
+    """
+    upload_model 保存模型参数到 hugging face hub
+    """
+    api = HfApi()  # 创建 Hugging Face API 实例
+    upload_file(  # 上传文件到 Hugging Face
+        path_or_fileobj="simple.bin",  # 本地文件路径
+        path_in_repo="simple.bin",  # 仓库中的文件名
+        repo_id="miniocean404/simple-nn",  # 仓库 ID
+        repo_type="model",  # 仓库类型为模型
+    )
